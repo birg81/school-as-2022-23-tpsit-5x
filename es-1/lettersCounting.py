25e69495296d0cc867d34fc6f9fe4a0295b99fea
Biagio Rosario Greco <32863770+birg81@users.noreply.github.com>
2022-09-20 13:48:09 +0200

    Add files via upload

---
+++ es-1/lettersCounting.py
@@ -0,0 +1,26 @@
+'''
+Creare una funzione che in ingresso prende una stringa
+e in uscita fornisce un dizionario che ha per chiavi le diverse lettere
+della stringa e per valori il conteggio delle diverse occorrenze
+es:
+INPUT: "CIAO ANNA"
+OUTPUT: {'C': 1, 'I': 1, 'A': 3, 'O': 1, ' ': 1, 'N':2}
+'''
+
+# funzione che effettua il conteggio
+def lettersCounter(world = ''):
+	#dizionario che contiene le lettere le occorrenze
+	counting = {}
+'''
+si scorre la stringa lettera per lettera, memorizzandole in la
+se l è già presente nel dizionario il valore viene incrementato
+altrimenti si crea una nuova chiave con valore 1
+'''
+	for l in world:
+		if l in counting:
+			counting[l] += 1
+		else:
+			counting[l] = 1
+	return counting
+
+print(lettersCounter('Forza Napoli'))
