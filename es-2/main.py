19437fe4d547bb1417f14b75f57978c427475b09
Biagio Rosario Greco <32863770+birg81@users.noreply.github.com>
2022-09-21 13:43:34 +0200

    Update main.py

--- es-2/main.py
+++ es-2/main.py
@@ -1,11 +1,25 @@
+'''
+Codifico e decodifico un messaggio secondo il cifrario di cesare
+'''
+
+# importo le funzioni per la cifratura e decodidica del messaggio
 from encDecMsg import encode, decode
+# importo la libreria per prelevare parametri da command line
 import sys
 
+# se sono presenti parametri da riga di comando li utilizzo
 if len(sys.argv) > 1:
+	# .join() unisce gli elementi di un array in una stringa
+	# il primo argomento del cmd line è il file stesso
+	# il primo argomento non mi serve, quindi lo filtro
 	msg = ' '.join(sys.argv[1:])
 else:
+	# se non ho inserito parametri chiedo il messaggio in input
 	msg = input('Insert your message: ').strip()
 
+# stampo dunque sia il messaggio originale
+# sia il messaggio codificato
+# sia la decodifica del messaggio codificato
 print(
 f'''
 * msg:	{msg}
