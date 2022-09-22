6ded90553052d45b82cf7b5e2a895f576b637080
Biagio Rosario Greco <32863770+birg81@users.noreply.github.com>
2022-09-22 14:05:43 +0200

    Update encDecMsg.py

--- es-2/encDecMsg.py
+++ es-2/encDecMsg.py
@@ -1,15 +1,20 @@
 def cesare(word = '', key = 3):
+	# numero di simboli da codificare (26: lettere dell'alfabeto)
 	M = ord('Z') - ord('A') + 1
+	# per traslazioni di lunghezza maggiore di M le riporta nel range 0, M-1
+	key = key % M
+	# parola criptata
 	word_enc = word.strip().upper()
-	if (M + key) % M > 0:
+	# se key % M = 0 ovvero se key = 0, 26, 52 ecc non fa nulla
+	if key > 0:
 		word_enc = ''
 		for l in word.strip().upper():
+			# controlla se l è compresa nel mio alfabeto
 			if ord(l) >= ord('A') and ord(l) <= ord('Z'):
-				word_enc += chr(
-					ord('A') +
-					((ord(l) - ord('A') + key + M) % M)
-				)
+				# in tal caso codifica il carattere
+				word_enc += chr( ord('A') + ((ord(l) -ord('A') +key) %M) )
 			else:
+				# altrimenti lo prende invariato
 				word_enc += l
 	return word_enc
 
