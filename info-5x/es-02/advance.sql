5cc3141407e4938eed7028ab572e10d0f4493ef5
Biagio Rosario Greco <32863770+birg81@users.noreply.github.com>
2022-11-08 12:14:49 +0100

    Update advance.sql

--- info-5x/es-02/advance.sql
+++ info-5x/es-02/advance.sql
@@ -1,3 +1,4 @@
+-- Test Advance query
 SELECT
 	p0.lastname AS cognome,
 	p0.firstname AS nome,
@@ -7,33 +8,34 @@ SELECT
 	cl.name AS cityLive,
 	cj.name AS cityJob
 FROM
+-- city born
 	(people AS p0)
 		JOIN
 	(cities AS cb)
 		ON
 	bornplace = cb.id,
-
-	(people AS p1)
+-- city live
+	(people AS pl)
 		JOIN
 	(place2live AS l)
 	ON
-		l.people = p1.id
+		l.people = pl.id
 	JOIN
 		(cities AS cl)
 	ON
 		l.place = cl.id,
-
-	(people AS p2)
+-- city job
+	(people AS pj)
 		JOIN
 	(place2job AS j)
 	ON
-		j.people = p2.id
+		j.people = pj.id
 	JOIN
 		(cities AS cj)
 	ON
 		j.place = cj.id
 WHERE
-	p1.id = p0.id AND
-	p2.id = p0.id
+	pl.id = p0.id AND
+	pj.id = p0.id
 ORDER BY
 	cognome ASC;
