73b98a60d08694c6ac35ab8da534cd928da4ad1c
Biagio Rosario Greco <32863770+birg81@users.noreply.github.com>
2023-01-13 15:08:02 +0100

    Update streamclient.py

--- tpsit-5x/es-07/StreamSocket/streamclient.py
+++ tpsit-5x/es-07/StreamSocket/streamclient.py
@@ -2,14 +2,14 @@ import socket
 
 HOST = '127.0.0.1'
 PORT = 12345
-msg = input('[write] >_: ').encode().strip()
+msg = input('[write] >_: ').strip()
 
 # Create a TCP/IP socket
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
 s.connect((HOST, PORT))
-s.send(msg)
+s.send(msg.encode())
 
-print (f'''{HOST}:{PORT} send >> ${msg}''')
+print (f'{HOST}:{PORT} send >> {msg}')
 
 s.close()
