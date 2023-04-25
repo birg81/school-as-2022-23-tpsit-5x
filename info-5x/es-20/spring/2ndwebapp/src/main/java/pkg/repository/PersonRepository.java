9749a28b17a19076b46a70b18a077c3822616dc2
Biagio Rosario Greco <32863770+birg81@users.noreply.github.com>
2023-04-25 15:19:40 +0200

    Delete info-5x/es-20 directory

--- info-5x/es-20/spring/2ndwebapp/src/main/java/pkg/repository/PersonRepository.java
+++
@@ -1,5 +0,0 @@
-package pkg.repository;
-import org.springframework.data.jpa.repository.JpaRepository;
-import pkg.entity.Person;
-public interface PersonRepository extends JpaRepository<Person, Integer> {
-}
